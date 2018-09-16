352e3fb5a06ea8a9268ff527c1aa48d926decffa
InspAlgo <InspAlgo@gmail.com>
2018-09-16 13:32:50 +0800

    fix

--- MiniFS/mfs_coresystem.cpp
+++ MiniFS/mfs_coresystem.cpp
@@ -9,22 +9,22 @@
 #include "mini_file_system.h"
 #include <Windows.h>
 
-
 MiniFS::MiniFS()
 {
-	mount_flag = false;
+    mount_flag = false;
     SetConsoleTitle(L"Mini-FS");
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 
-        FOREGROUND_INTENSITY |  FOREGROUND_GREEN | FOREGROUND_BLUE);
-	std::cout << "Mini File System [版本 3.2.13]" << std::endl;
-	std::cout << "(c) 2018 AlphaBeta。保留所有权利。" << std::endl;
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                            FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    std::cout << "Mini File System [版本 3.2.13]" << std::endl;
+    std::cout << "(c) 2018 AlphaBeta。保留所有权利。" << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY);
 }
-
 
 MiniFS::~MiniFS()
 {
 }
+
+#ifdef SYSTEM_TEST
 
 void MiniFS::Test_createFile(int file_num)
 {
@@ -36,3 +36,5 @@ void MiniFS::Test_createFile(int file_num)
         this->createFile((char *)file_name.data());
     }
 }
+
+#endif
